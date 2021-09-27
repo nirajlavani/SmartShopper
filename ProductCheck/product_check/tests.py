0ce5a2d883760ef44f7ea45647090f467ad24101
Chandrahas Reddy Mandapati <chandrahasreddymandapati@Chandrahass-MBP.lan>
2021-09-27 00:30:25 -0400

    adding tests for defined models

--- ProductCheck/product_check/tests.py
+++ ProductCheck/product_check/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(product_url="https://www.amazon.com/", product_category="amazon", is_active=False)
+        Product.objects.create(product_url="https://www.walmart.com/", product_category="walmart", is_active=False)
+
+    def test_product_details(self):
+        """Created instances are retrieved and respective categories are verified"""
+        amazon_product = Product.objects.get(product_url="https://www.amazon.com/")
+        walmart_product = Product.objects.get(product_url="https://www.walmart.com/")
+        self.assertEqual(amazon_product.product_category, "amazon")
+        self.assertEqual(walmart_product.product_category, "walmart")
